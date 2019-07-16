b3f11cd409c3c298730e6d66a5edcf81f34ae32f
Vincenzo <52751334+brearlycoffee@users.noreply.github.com>
2019-07-16 17:11:36 +0200

    Added borders

--- hub-young-downloader.py
+++ hub-young-downloader.py
@@ -1,6 +1,7 @@
 import os
 import shutil
-from PIL import Image
+from PIL import Image, ImageOps
+import cv2 as cv
 
 ##########################################################################
 # CUSTOM SETTINGS: edit this BEFORE running the program.
@@ -46,8 +47,8 @@ while notvalid:
     else:
         print("Insert a valid book directory.")
 
-book_path = book_path +codein+"\\"
-out_path = out_path + codein+"\\"
+book_path = book_path + codein + "\\"
+out_path = out_path + codein + "\\"
 os.mkdir(out_path)
 
 # publication directory may contain images named like others files in other directories
@@ -63,20 +64,26 @@ print("Selected book: "+str(codein))
 print("Source directory: "+book_path)
 print("Destination directory: "+out_path)
 count = 0
+maxwidth = 0
+maxheight = 0
 for root, dirs, files in os.walk(book_path):
     for file in files:
         path_file = os.path.join(root,file)
         try:
             page = Image.open(path_file)
-        except:
+        except: # the file is not an image
             pass
         else: 
-            # acquisiring width and height of every image
+            # acquiring width and height of every image
             width, height = page.size
             if width >= minwidth and height >= minheight:
+                if width > maxwidth:
+                    maxwidth = width
+                if height > maxheight:
+                    maxheight = height
                 try:
                     shutil.copy2(path_file,out_path)
-                except: # the file is not an image
+                except: 
                     pass
                 else:
                     count+=1
@@ -91,5 +98,41 @@ try:
     shutil.move("C:\\Users\\user\\AppData\\Local\\HUB young\\publication",book_path)
 except:
     print("Failed!")
+else:
+    print("Done.")
+
+print("\nApplying borders to pages...")
+print("Page size: "+str(maxwidth)+" x "+str(maxheight))
+WHITE = [255,255,255]
+
+imagelist = os.listdir(out_path)
+for i in range(len(imagelist)):
+    # stored image name
+    name = imagelist[i]
+    # imagelist[i] now contains the full path.
+    imagelist[i] = out_path+imagelist[i]
+    try:
+        page = Image.open(imagelist[i])
+    except: # the file is not an image
+        pass
+    else:
+        width, height = page.size
+        
+        # calculating borders
+        top = int((maxheight-height)/2)
+        bottom = top
+        if (maxheight-height)%2 != 0:
+            top +=1
+        left = int((maxwidth-width)/2)
+        right = left
+        if (maxwidth-width)%2 != 0:
+            left +=1
+        
+        if top != 0 or bottom != 0 or left != 0 or right != 0:   
+            src = cv.imread(cv.samples.findFile(imagelist[i]), cv.IMREAD_COLOR)
+            dst = cv.copyMakeBorder(src, top, bottom, left, right, cv.BORDER_REPLICATE, None, WHITE)
+            #dst = cv.copyMakeBorder(src, top, bottom, left, right, cv.BORDER_CONSTANT, None, WHITE)
+            cv.imwrite(out_path+name,dst)
+
 print("Done. Check: "+out_path)
 
